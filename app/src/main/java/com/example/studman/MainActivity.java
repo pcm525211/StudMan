2476dfecb4be23b8d1726fe63006b9afe361de07
Vishnu <vkantliwala@gmail.com>
2020-03-18 16:44:52 +0530

    Home page new look

--- app/src/main/java/com/example/studman/MainActivity.java
+++ app/src/main/java/com/example/studman/MainActivity.java
@@ -3,16 +3,18 @@ package com.example.studman;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import com.android.volley.RequestQueue;
 
 public class MainActivity extends AppCompatActivity {
 
     //declarations
-    Button btn,btn2,btnInstitutes,btnCourses;
+
+    CardView btnIns;
+    CardView btnCourses;
     RequestQueue queue;
 
 
@@ -24,14 +26,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        btnInstitutes = (Button) findViewById(R.id.btn_institutes);
-        btnCourses = (Button) findViewById(R.id.btn_courses);
+
+        btnIns = (CardView)  findViewById(R.id.btn_ins);
+        btnCourses = (CardView) findViewById(R.id.btn_courses);
 
         //go to institutes
 
-        btnInstitutes.setOnClickListener(new View.OnClickListener() {
+        btnIns.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),Institutes.class);
                 startActivity(i);
             }
