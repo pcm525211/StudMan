31d0deaa95d4b6120c44b6264a2eaddad03f9454
Vishnu <vkantliwala@gmail.com>
2020-03-20 00:43:35 +0530

    tablayout added..simple yet good Also used matrial.io

--- app/src/main/java/com/example/studman/Exams.java
+++ app/src/main/java/com/example/studman/Exams.java
@@ -1,35 +1,87 @@
 package com.example.studman;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-import com.example.studman.ui.main.SectionsPagerAdapter;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Exams extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private Toolbar toolbar;
+    private UpcomingExams upcomingExams;
+    private CompleteExams completeExams;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setTitle("Exams");
+        toolbar.setTitleTextColor(getColor(R.color.white));
+
+        completeExams = new CompleteExams();
+        upcomingExams = new UpcomingExams();
+
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setBackgroundColor(getColor(R.color.white));
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),0);
+        viewPagerAdapter.addFragment(upcomingExams,"Upcoming Exams");
+        viewPagerAdapter.addFragment(completeExams,"Complete Exams");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        public List<Fragment> fragmentList = new ArrayList<>();
+        public List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+            super(fm, behavior);
+        }
+
+        public void addFragment(Fragment fragment, String title)
+        {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return  fragmentTitleList.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
 }
