37ab0cb7530f6fb507ef44167ae748d688866b4a
leancer <kamiyabasamadi@gmail.com>
2020-04-19 03:53:51 +0530

    UNTESTED : add Update Profle Dialog

--- app/src/main/java/com/example/studman/Profile.java
+++ app/src/main/java/com/example/studman/Profile.java
@@ -1,14 +1,20 @@
 package com.example.studman;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,13 +29,16 @@ import com.bumptech.glide.Glide;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Profile extends AppCompatActivity {
 
     private ProgressDialog progressDialog;
     CircleImageView usericon,courseicon;
-    TextView username,coursename,enrolldate,email;
+    TextView username,coursename,enrolldate,email,imageName;
+    private int PICK_PROFILE_IMAGE_REQUEST = 1;
     SQLiteDatabase db;
     String userid;
     String URL = "https://www.leancerweb.com/studman/profile/index.php?";
@@ -96,8 +105,60 @@ public class Profile extends AppCompatActivity {
         requestQueue.add(jsonObjReq);
     }
 
-    public void onProfileEdit(View view){
+    public void onProfilePicEdit(View view){
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.upload_image_dialog, null);
+        imageName = alertLayout.findViewById(R.id.txt_slectedImage);
+        final Button selectButton = alertLayout.findViewById(R.id.btn_selectimage);
 
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Update Image");
+        alert.setView(alertLayout);
+        alert.setCancelable(false);
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               dialog.dismiss();
+            }
+        });
+        alert.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getBaseContext(),"hello",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+
+        selectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+    }
+
+    private void selectImage(){
+        Intent intent= new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), PICK_PROFILE_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode,data);
+
+        if(requestCode == PICK_PROFILE_IMAGE_REQUEST && resultCode ==
+                RESULT_OK && data != null && data.getData() != null) {
+            Uri filePath = data.getData();
+            File file= new File(filePath.getPath());
+            imageName.setText(file.getName());
+        }
     }
 
     public void onLogoutButton(View v){
